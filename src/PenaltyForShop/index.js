8d7d723099f9a7f95a841b8668b7393aa989121c
naresh <nareshm1997kumar@gmail.com>
2024-01-27 22:45:47 +0530

    chore():commit frim Inida

--- src/PenaltyForShop/index.js
+++ src/PenaltyForShop/index.js
@@ -1,6 +1,30 @@
 const input = "YYYY";
+let zerothHourSum = 0;
 const penaltyArray = [];
+const calculatedPenalty = [];
 
 for (let index = 0; index < input.length; index++) {
-    // if(input.charAt(index))
+  if (input.charAt(index) === "Y") {
+    penaltyArray.push(1);
+    zerothHourSum += 1;
+  } else {
+    penaltyArray.push(0);
+  }
 }
+
+for (let index = 0; index < input.length; index++) {
+  calculatedPenalty.push(
+    penaltyArray.reduce((acc, cur, penIndex) => {
+      acc += penIndex <= index ? (cur === 0 ? 1 : 0) : 0;
+      if (penIndex > index) {
+        acc += cur;
+      }
+      return acc;
+    }, 0)
+  );
+}
+
+console.log(penaltyArray);
+console.log(zerothHourSum, calculatedPenalty);
+
+console.log(Math.min(zerothHourSum));
